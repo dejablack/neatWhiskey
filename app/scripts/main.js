42463b948e415bd01647500e7f74a07a4fef20cd
dejablack <tschultztt@gmail.com>
2014-07-01 12:10:04 -0500

    misc, need to go

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,7 +11,7 @@ console.log('\'ello from creativo!');
 ///
 ///
 var $window = $(window)
-var $box = $(".boxColor");
+var $box = $(".boxPosistion");
 var $menus = $(".headerTitle ul");
 
 var currentIndex = 0;
